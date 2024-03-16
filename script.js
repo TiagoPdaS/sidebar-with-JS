7278b48b3c208eccfc7711ead9f0ec15f3a9de73
myuser <myemail>
2024-03-16 14:50:43 -0400

    simple sidebar

---
+++ script.js
@@ -0,0 +1,6 @@
+const sidebar = document.getElementById('sidebar');
+const button = document.getElementById('btn');
+
+button.addEventListener('click', function(){
+sidebar.classList.toggle('fechar');
+});
